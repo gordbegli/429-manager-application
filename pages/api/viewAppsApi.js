061fff4ebc64944a0c8f0b6660a8025c2da886d5
ekindseth <82418428+ekindseth@users.noreply.github.com>
2023-05-04 13:50:11 -0400

    Revert "temp"
    This reverts commit 7eefdfac0f070b8f2dc381ce22fec57e3bb2b43b.

--- pages/api/viewAppsApi.js
+++ pages/api/viewAppsApi.js
@@ -1,21 +1,5 @@
-import database from "../../lib/Firebase";
-import { ref, onValue, child } from "firebase/database";
+import '@/styles/globals.css'
 
-export const fetchApplicants = async () => {
-  return new Promise((resolve, reject) => {
-    const applicantsRef = ref(database, "studentApplications");
-    onValue(
-      applicantsRef,
-      (snapshot) => {
-        const data = snapshot.val();
-        const applicants = Object.keys(data).map((key) => ({
-
-        }));
-        resolve(applicants);
-      },
-      (error) => {
-        reject(error);
-      }
-    );
-  });
-};
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
