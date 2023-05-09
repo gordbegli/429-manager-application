350b3e5ee3ccbe085880e5c3846333fb9032b80c
joshuabornstein <jhbornstein@umass.edu>
2023-05-09 15:17:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    // Replace this with your autexhentication logic
+    router.push('/login'); 
+  }, []);
   return (
     <>
       <Head>
