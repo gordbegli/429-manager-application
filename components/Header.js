30a395609c02e4e1dc1a7123ab4eb10de8187355
Brijesh Vyas <developerjesh@gmail.com>
2023-05-11 15:46:36 -0400

    creating pending and accepting pages

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,30 @@ import AddBusinessRoundedIcon from "@mui/icons-material/AddBusinessRounded";
 import DrawerComp from "./Drawer";
 import { BrowserRouter, Link } from 'react-router-dom';
 
+import { ref, onValue, child } from "firebase/database"; //dont think i need this
+
+import { useRouter } from "next/router";
+
 const Header = () => {
   const [value, setValue] = useState();
   const theme = useTheme();
   console.log(theme);
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   console.log(isMatch);
+
+  const router = useRouter()
+
+  const handleAllApplicantClick = () => {
+    router.push('./viewApplications')
+  };
+
+  const handlePendingApplicantClick = () => {
+    router.push('./viewPendingApplications')
+  };
+
+  const handleAcceptedApplicantClick = () => {
+    router.push('./viewAcceptedApplications')
+  };
 
   return (
     <React.Fragment>
@@ -41,9 +59,9 @@ const Header = () => {
                 value={value}
                 onChange={(e, value) => setValue(value)}
               >
-                <Tab label="All Applications"/>
-                <Tab label="Accepted Applications"/>
-                <Tab label="Pending Applications"/>
+                <Tab label="All Applications" onClick={handleAllApplicantClick}/>
+                <Tab label="Accepted Applications" onClick={handleAcceptedApplicantClick}/>
+                <Tab label="Pending Applications" onClick={handlePendingApplicantClick}/>
               </Tabs>
               {/* <Button sx={{ marginLeft: "auto" }} variant="contained">
                 Login
